02ee62520adc2082d4a1724b4143f3510b9417d4
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-30 20:31:50 +0100

    45 RecyclerView implemented for to display Strings with TextView layout for rows

--- app/src/main/java/com/example/listview45/MainActivity.java
+++ app/src/main/java/com/example/listview45/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.listview45;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // https://developer.android.com/guide/topics/ui/layout/recyclerview#java
+    // declare vars to do with RecyclerView
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +44,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        String[] foods = {"Chips","Fish (battered)","Peas","Vinegar","Salt","Screeds"};
+        String[] foods = {"Chips","Fish (battered)","Peas","Vinegar","Salt","Screeds", "Tommy Tucker", "Reluctant Salad", "Bread & Butter"};
         // simple form of list data, starting with plain strings - need to be turned into list items
         // adapter to turn array members to list items
 
+        recyclerView = (RecyclerView) findViewById(R.id.pnjRecyclerView);
 
+        // performance improvment if RecyclerView known to not vary in size
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager (huh?)
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter
+        mAdapter = new PNJRecyclerAdapter(foods);       // specify also a set of data
+        recyclerView.setAdapter(mAdapter);
 
     }
 
@@ -57,4 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // could not define RecyclerView.Adapter class nested/inner here ...
+    // ... as static members not allowed in inner classes (therefore, in separate file)
 }
---
+++ app/src/main/java/com/example/listview45/PNJRecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.example.listview45;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+// class defined in separate file because static members cannot exist in inner/nested classes
+// https://stackoverflow.com/a/31957978/11365317
+// also, some people d'prefer each class to have its own file, where possible
+// class consumed in MainActivity for intended app
+
+public class PNJRecyclerAdapter extends RecyclerView.Adapter<PNJRecyclerAdapter.PNJViewHolder>
+{
+    private String[] mDataset;
+
+    // reference to view(s) for each data item
+    public static class PNJViewHolder extends RecyclerView.ViewHolder
+    {
+        // where data items are: strings
+        public TextView textView;
+        public PNJViewHolder(TextView v)
+        {
+            super(v);       // in constructor - do what TextView d'do
+            textView = v;
+        }
+    }
+
+    public PNJRecyclerAdapter(String[] pnjDataset)
+    {
+        mDataset = pnjDataset;
+    }
+
+    // for news views (this invoked by the layout manager in code)
+    // Alt+Insert + overrides...
+    @NonNull
+    @Override
+    public PNJViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//            return null;
+        TextView textView = (TextView) LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.pnj_text_view, viewGroup, false);
+        PNJViewHolder viewHolder = new PNJViewHolder(textView);
+        return viewHolder;
+    }
+
+    // Alt+Insert again - used by layout manager automatically
+    @Override
+    //public void onBindViewHolder(@NonNull PNJViewHolder holder, int position, @NonNull List<Object> payloads) {
+    // NB this boilerplate signature with 3 parameters will cause an error with implementing ...
+    // ... RecyclerView Adapter, ... which d'require onBindViewHolder to have 2 parameters only
+
+    public void onBindViewHolder(@NonNull PNJViewHolder holder, int position) {
+        //super.onBindViewHolder(holder, position, payloads);
+        holder.textView.setText(mDataset[position]);
+        // cf cellForRowAt in iOS/Swift
+    }
+
+    @Override
+    public int getItemCount() {
+        // return 0;
+        return mDataset.length;
+        // cf NumRows or similar in iOS/Swift
+    }
+}
